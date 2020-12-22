21fdeb431d7c78c0bac2f7cb850d7981834e4d44
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-12-22 03:46:40 +0300

    sta@PrimaryKey

---
+++ app/src/main/java/kapadokia/nyandoro/room_livedata_and_viewmodel/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package kapadokia.nyandoro.room_livedata_and_viewmodel.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kapadokia.nyandoro.room_livedata_and_viewmodel.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
